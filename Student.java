a61bc94f43ee07d75ef39a9a727f71429897a530
RidwanulHasan <36354494+RidwanulHasan@users.noreply.github.com>
2020-02-23 23:27:27 +0600

    Readme

---
+++ Student.java
@@ -0,0 +1,52 @@
+
+public class Student {
+	
+	
+	private String name;
+	private String id;
+	private double cgpa;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public double getCgpa() {
+		return cgpa;
+	}
+	public void setCgpa(double cgpa) {
+		this.cgpa = cgpa;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", id=" + id + ", cgpa=" + cgpa + "]";
+	}
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+		
+		
+	
+
+
+
+
+
+
+	
---
+++ Studenttest.java
@@ -0,0 +1,24 @@
+
+public class Studenttest {
+
+	public static void main(String[] args) {
+		
+		
+		Student s1=new Student();
+		s1.setName("My name is Hridoy");
+		s1.setId("17-35873-3");
+		s1.setCgpa(3.5);
+		
+		System.out.println(s1);
+		
+		Student s2=new Student();
+		s2.setName("Rasel");
+		s2.setId("17-35886-3");
+		s2.setCgpa(3.4);
+		
+		
+		System.out.println(s2);
+		
+	}
+
+}
